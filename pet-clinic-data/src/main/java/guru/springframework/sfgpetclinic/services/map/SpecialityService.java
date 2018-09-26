5850243c9259b68c1a26ba4757e10dd2681cd50c
MehmetAyan <mehmetayan84@gmail.com>
2018-09-26 23:53:23 +0300

    Speciality map service is implemented. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}

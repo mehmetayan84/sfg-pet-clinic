16cffae05314a72d66249c6d41e1e4e9df46707c
MehmetAyan <mehmetayan84@gmail.com>
2018-11-27 00:20:59 +0300

    Upgraded to Java11 to solve CircleCI issue.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -108,7 +108,7 @@ class OwnerControllerTest {
 
         mockMvc.perform(post("/owners/new"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect: /owners/1"))
+                .andExpect(view().name("redirect:/owners/1"))
                 .andExpect(model().attributeExists("owner"));
 
         verify(ownerService).save(ArgumentMatchers.any());
@@ -135,7 +135,7 @@ class OwnerControllerTest {
 
         mockMvc.perform(post("/owners/1/edit"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect: /owners/1"))
+                .andExpect(view().name("redirect:/owners/1"))
                 .andExpect(model().attributeExists("owner"));
 
         verify(ownerService).save(ArgumentMatchers.any());
